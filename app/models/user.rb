87b58981374d04ae0f07ec45a1c96ef1ea721f6d
Ifeanyi <ikalu13@gmail.com>
2014-08-18 21:38:53 -0400

    fix minor error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true,
-    format: { with: /\A([^@\s]+)@((?:claflin+\.)+edu)\z/i }
+  validates(
+    :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A.+@claflin\.edu\z/i }
+  )
+  before_save :downcase_data
+
 
   validates :password_digest, presence: true
+
+
+  private
+  def downcase_data
+    self.email = email.downcase
+  end
 end
