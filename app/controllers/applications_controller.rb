85d8e3cdb51d091a66a417e9ee44c148d4745afc
Ifeanyi <ikalu13@gmail.com>
2014-08-30 03:47:21 -0400

    Modify Admin::application show page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,10 +7,10 @@ class ApplicationsController < ApplicationController
     application = Application.new(application_params)
 
     if application.save
-      flash[:notice] = "Application submitted successfully"
+      flash[:notice] = "Application submitted successfully."
       redirect_to dashboard_path
     else
-      flash[:alert] = "Application cannot be submitted"
+      flash.now[:alert] = "Application cannot be submitted."
       render :new
     end
   end
@@ -20,7 +20,7 @@ class ApplicationsController < ApplicationController
   def application_params
     params.
       require(:application).
-      permit(:academic_year, :semester, :home_address, :home_phone, :student_id, :age, :date_of_birth, :gender, :parent_name, :parent_address, :parent_home_telephone, :parent_work_telephone, :roommate_preference, :roommate1, :roommate2, :roommate3, :any_disability, :disability_description, :previous_application, :acknowledgment, student_status_ids: [], hall_ids: []).
+      permit(:academic_year, :semester, :home_address, :home_phone, :student_id, :age, :date_of_birth, :gender, :parent_name, :parent_address, :parent_home_telephone, :parent_work_telephone, :roommate_preference, :roommate1, :roommate2, :roommate3, :any_disability, :disability_description, :previous_application, :acknowledgement, student_status_ids: [], hall_ids: []).
       merge(user: current_user)
   end
 end
