b4d47275e05c2bd9278cd354bb980b368197d790
Ifeanyi <ikalu13@gmail.com>
2015-01-24 13:48:53 -0500

    Fix vendor/assets path

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,8 @@ module Housng
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    # config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    # config.assets.precompile += %w( .svg .eot .woff .ttf )
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
